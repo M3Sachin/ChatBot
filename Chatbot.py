dd5ab6c3de15b84c1b598c8b4b63b51bddf87076
Sachin Kumar <sk4830812@gmail.com>
2025-04-10 15:14:38 +0530

    testing for bot

--- Chatbot.py
+++ Chatbot.py
@@ -153,7 +153,7 @@ txt = Text(root)
 txt.grid(row = 0,column = 0, columnspan = 2)
 
 e = Entry(root,width = 100)
-send = Button(root,text = "Send",command=send).grid(row = 1,column = 1)
+Button(root, text="Send", command=send).grid(row=1, column=1)
 e.grid(row = 1,column = 0)
 
 root.mainloop()
